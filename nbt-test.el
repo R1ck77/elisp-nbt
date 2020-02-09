dcfef30fb54972228fcf2c4cdd05122e7c24cbdc
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-02-09 18:44:41 +0100

    Update nbt/read-uncompressed-file test (!test)

--- nbt-test.el
+++ nbt-test.el
@@ -5,10 +5,10 @@
 
 
 (describe "nbt"
-  (xdescribe "nbt/read-uncompressed-file"
+  (describe "nbt/read-uncompressed-file"
     (it "can read the basic test"
       (let ((expected-result (make-nbt-compound :name "hello world"
                                                 :items (list (make-nbt-string :name "name"
                                                                          :value "Bananrama")))))
-        (expect (nbt-read-uncompressed-file "test-data/hello_world.nbt")
+        (expect (nbt/read-uncompressed-file "test-data/hello_world.nbt")
                 :to-equal expected-result)))))
--- nbt.el
+++ nbt.el
@@ -1,13 +1,19 @@
+(require 'cl)
 (require 'nbt-tags)
 (require 'bindat)
+
+(cl-defstruct nbt-compound name items)
+
+(defun nbt/reduce-tags (raw-tags)
+  
+  )
 
 
 (defun nbt/read-uncompressed-file (path)
   (with-temp-buffer
+    (toggle-enable-multibyte-characters nil)
     (insert-file-contents-literally path)
     (goto-char (point-min))
-    
-    )
-  )
+    (nbt/reduce-tags (nbt/read-all-raw-tags))))
 
 (provide 'nbt)
