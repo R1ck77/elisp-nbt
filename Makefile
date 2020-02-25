dd6fedd39b58c8abee67e29ce550cd2a04b6dfa0
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-02-25 16:54:20 +0100

    Add a C module stub

--- Makefile
+++ Makefile
@@ -1,5 +1,15 @@
-.PHONY: test
+.PHONY: test clean run
 
-test:
+EMACS_INCLUDE_FOLDER=-I${HOME}/local/emacs/include
+
+run: test
+
+# TODO/FIXME this works only on specific systems. Check the joystick demo for a more general approach
+libnbt.so: nbt.c
+	gcc $(EMACS_INCLUDE_FOLDER) -shared -fPIC -o $@ nbt.c
+
+test: libnbt.so
 	emacs --eval "(setq load-path (cons \".\" load-path))" -batch -f package-initialize -f buttercup-run-discover
 
+clean:
+	rm -f libnbt.so
--- nbt-ieee754.el
+++ nbt-ieee754.el
@@ -1,15 +1,22 @@
 ;;; C-supported routines for floating point conversion
 
-(defun ensure-module-file-support ()
+(defun nbt/ensure-module-file-support ()
   (if (not module-file-suffix)
       (error "Emacs should support floating point modules for this package to work")))
 
+(defun nbt/ensure-module-file-present ()
+  (module-load "./libnbt.so"))
+
+(defun nbt/check-environment ()
+  (nbt/ensure-module-file-support)
+  (nbt/ensure-module-file-present))
+
 (defun nbt/convert-bytes-to-float (bytes)
-  (ensure-module-file-support)
+  (nbt/check-environment)
   0.0)
 
 (defun nbt/convert-bytes-to-double (bytes)
-  (ensure-module-file-support)
+  (nbt/check-environment)
   0.0)
 
 (provide 'nbt-ieee754)
