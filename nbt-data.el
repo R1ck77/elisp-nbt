30da6bb34c77e45a895358327ad5259ffbddf91d
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-03-01 10:56:12 +0100

    Replace multiple byte read with single multi-byte calls

--- nbt-data.el
+++ nbt-data.el
@@ -2,31 +2,43 @@
 (require 'dash)
 (require 'nbt-native)
 
-(defun nbt/read--binary-value (size type)
-  (let ((result (bindat-unpack (list (list 'unused-name type))
-                               (buffer-substring-no-properties (point) (+ size (point))))))    
-    (goto-char (+ size (point)))
-    (cdr (car result))))
+(defun nbt/create--spec (type count)
+  `((unused-name vec ,count ,type)))
+
+(defun nbt/vector-to-list (vector)
+  (append vector nil))
+
+(defun nbt/read--binary-values (type count)
+  (let* ((spec (nbt/create--spec type count))
+         (spec-length (bindat-length spec nil)))
+    (let ((result (bindat-unpack spec
+                                 (buffer-substring-no-properties (point) (+ spec-length (point))))))
+      (goto-char (+ spec-length (point)))
+      (cdr (car result)))))
+
+(defun nbt/read--binary-value (type &optional count)
+  (nbt/read--binary-values type (or count 1)))
 
 (defun nbt/read-byte ()
-  (nbt/read--binary-value 1 'byte))
+  (elt (nbt/read--binary-value 'byte) 0))
 
 (defun nbt/read-short ()
-  (nbt/read--binary-value 2 'short))
+  (elt (nbt/read--binary-value 'short) 0))
 
 (defun nbt/read-int ()
-  (nbt/read--binary-value 4 'long))
+  (elt (nbt/read--binary-value 'long) 0))
 
-;;; TODO/FIXME can be probably read with a single bindat call
+(defun nbt/read--bytes (count)
+  (append (nbt/read--binary-value 'byte count) nil))
+
 (defun nbt/read-long ()
-  (nbt/convert-bytes-to-long (--map (nbt/read-byte) (number-sequence 1 8))))
+  (nbt/convert-bytes-to-long (nbt/read--bytes 8)))
 
-;;; TODO/FIXME can be probably read with a single bindat call
 (defun nbt/read-float ()
-  (nbt/convert-bytes-to-float (--map (nbt/read-byte) (number-sequence 1 4))))
+  (nbt/convert-bytes-to-float (nbt/read--bytes 4)))
 
 (defun nbt/read-double ()
-  (nbt/convert-bytes-to-double (--map (nbt/read-byte) (number-sequence 1 8))))
+  (nbt/convert-bytes-to-double (nbt/read--bytes 8)))
 
 (defun nbt/read-string ()
   (let* ((string-length (nbt/read-short))
